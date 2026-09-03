e5547be7612f168bc705909d92fa1e4beee37853
Casas Rodrigo <casasperezrodrigoimanol2501@gmail.com>
2026-09-03 11:09:38 -0600

    Personalizo el saludo usando argumentos

--- practicas/3/CasasRodrigo/saludo.c
+++ practicas/3/CasasRodrigo/saludo.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Hola mundo\n");
+    if (argc < 2) {
+        printf("Uso: %s <nombre>\n", argv[0]);
+        return 1;
+    }
+
+    printf("Hola, %s!\n", argv[1]);
     return 0;
 }
